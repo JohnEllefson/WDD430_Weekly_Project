001572246f6b3884cd067ad6ee4cee66ed40696e
John Ellefson <johnellefson@byui.edu>
2025-05-16 14:08:07 -0500

    Week 4 final commit. Finished debugging the document components so now the detail view properly accepts the document that was clicked on in the document list component.

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -7,5 +8,47 @@ import { Component } from '@angular/core';
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
+  documents: Document[] = [
+    {
+      id: 1,
+      name: 'Sample Document 1',
+      description: 'This is sample document 1 description.',
+      url: 'https://example.com/sample-document1.pdf',
+      children: []
+    },
+    {
+      id: 2,
+      name: 'Sample Document 2',
+      description: 'This is sample document 2 description.',
+      url: 'https://example.com/sample-document2.pdf',
+      children: []
+    },
+    {
+      id: 3,
+      name: 'Sample Document 3',
+      description: 'This is sample document 3 description.',
+      url: 'https://example.com/sample-document3.pdf',
+      children: []
+    },
+    {
+      id: 4,
+      name: 'Sample Document 4',
+      description: 'This is sample document 4 description.',
+      url: 'https://example.com/sample-document4.pdf',
+      children: []
+    },
+    {
+      id: 5,
+      name: 'Sample Document 5',
+      description: 'This is sample document 5 description.',
+      url: 'https://example.com/sample-document5.pdf',
+      children: []
+    }
+  ];
+
+  onSelectDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
