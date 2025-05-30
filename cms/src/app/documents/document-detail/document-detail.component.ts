9119d76ab1dd3f15f092d0950bf1a74ffbaf6454
John Ellefson <johnellefson@byui.edu>
2025-05-30 18:25:11 -0500

    Week 6 final update. Finished debugging the contacts detail page which wasn't pointing to the correct route. All features now working.

--- cms/src/app/documents/document-detail/document-detail.component.ts
+++ cms/src/app/documents/document-detail/document-detail.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
+import { WindRefService } from '../../wind-ref.service';
 
 @Component({
   selector: 'cms-document-detail',
@@ -7,6 +11,40 @@ import { Document } from '../document.model';
   templateUrl: './document-detail.component.html',
   styleUrl: './document-detail.component.css'
 })
-export class DocumentDetailComponent {
-  @Input() document: Document;
+export class DocumentDetailComponent implements OnInit {
+  document: Document;
+  nativeWindow: any;
+
+  constructor(
+    private documentService: DocumentService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private windowRefService: WindRefService
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      const id = params['id'];
+      this.document = this.documentService.getDocument(id);
+    });
+
+    this.nativeWindow = this.windowRefService.getNativeWindow();
+  }
+
+  onView() {
+    if (this.document.url) {
+      this.nativeWindow.open(this.document.url);
+    } else {
+      alert('No URL found for this document.');
+    }
+  }
+
+  onDelete() {
+    if (this.document) {
+      this.documentService.deleteDocument(this.document);
+      this.router.navigate(['/documents']);
+    } else {
+      alert('No document selected for deletion.');
+    }
+  }
 }
--- cms/src/app/documents/document.service.ts
+++ cms/src/app/documents/document.service.ts
@@ -7,6 +7,7 @@ import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
 })
 export class DocumentService {
   documentSelectedEvent = new EventEmitter<Document>();
+  documentChangedEvent = new EventEmitter<Document[]>();
   documents: Document[] = [];
 
   constructor() {
@@ -19,5 +20,19 @@ export class DocumentService {
 
   getDocument(id: number): Document | undefined {
     return this.documents.find((document) => document.id === id);
+  }
+
+  deleteDocument(document: Document): void {
+    if (!document) {
+      return;
+    }
+
+    const pos = this.documents.indexOf(document);
+    if (pos < 0) {
+      return;
+    }
+
+    this.documents.splice(pos, 1);
+    this.documentChangedEvent.emit(this.documents.slice());
   }
 }
